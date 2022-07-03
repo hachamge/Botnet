ed627a83c3c3b51c784b1a854815062187f052d4
Seymour Roberts <seymour.roberts@seeq.com>
2022-07-02 21:20:00 -0700

    Input operations added along .gitignore file

--- botnet.cpp
+++ botnet.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std
 
 // Sample git tutorial prep ... 
 int main() {
+	string param;
+	cout << "// Enter a param: ";
+	getline(cin, param);
+	cout << "param: " << param << endl;
 	return 0;
 }
